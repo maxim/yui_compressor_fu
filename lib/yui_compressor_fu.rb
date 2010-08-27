005114dd631db9e8131e243743b19d414a824e69
Maxim Chernyak <max@bitsonnet.com>
2010-08-27 15:43:14 -0400

    Update yui_compressor to HEAD on master (2.4.4).

--- lib/yui_compressor_fu.rb
+++ lib/yui_compressor_fu.rb
@@ -1,5 +1,5 @@
 module YuiCompressorFu
-  YUI_COMPRESSOR_VERSION = '2.4.2'
+  YUI_COMPRESSOR_VERSION = '2.4.4'
 end
 
 require File.join(File.dirname(__FILE__), "yui_compressor_fu", "compressor.rb")
